cca617860d0b5767b997315bead50bd68e3cdceb
qwe8851 <dgh07027@naver.com>
2022-11-06 00:28:36 +0900

    변수의 Hoisting
    hoisting, 전역변수, 참고 알아보기

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // var let const 선언,할당,범위
 
-
-
-// var   재선언 O 재할당 O
-// let   재선언 X 재할당 O
-// const 재선언 X 재할당 X
+function 함수() {
+    var a = 1;
+    let b = 2;
+    if (true) {
+        let b = 3;
+    }
+    console.log(b);  // 2
+}
+console.log(a);  //에러
+함수();
